bc81f168945f84d6f5c8e91c18c5fc7c70b12ae3
iwsylit <shawolaaa@gmail.com>
2024-09-13 23:37:51 +0300

    add audio class

---
+++ simulst/audio.py
@@ -0,0 +1,97 @@
+from typing import Self
+
+import miniaudio
+import numpy as np
+
+
+class Audio:
+    _SAMPLE_FORMAT = miniaudio.SampleFormat.SIGNED16
+    _SAMPLE_RATE = 16000
+    _NCHANNELS = 1
+
+    def __init__(self, audio: miniaudio.DecodedSoundFile) -> None:
+        self._audio = audio
+
+    @classmethod
+    def from_bytes(
+        cls,
+        bytes: bytes,
+        nchannels: int = _NCHANNELS,
+        sample_rate: int = _SAMPLE_RATE,
+        sample_format: miniaudio.SampleFormat = _SAMPLE_FORMAT,
+    ) -> Self:
+        audio = miniaudio.decode(
+            bytes,
+            nchannels=nchannels,
+            sample_rate=sample_rate,
+            output_format=sample_format,
+        )
+
+        return cls(audio)
+
+    @classmethod
+    def from_file(
+        cls,
+        filename: str,
+        nchannels: int = _NCHANNELS,
+        sample_rate: int = _SAMPLE_RATE,
+        sample_format: miniaudio.SampleFormat = _SAMPLE_FORMAT,
+    ) -> Self:
+        audio = miniaudio.decode_file(
+            filename,
+            nchannels=nchannels,
+            sample_rate=sample_rate,
+            output_format=sample_format,
+        )
+
+        return cls(audio)
+
+    @classmethod
+    def fake(
+        cls,
+        nsamples: int,
+        nchannels: int = _NCHANNELS,
+        sample_rate: int = _SAMPLE_RATE,
+        sample_format: miniaudio.SampleFormat = _SAMPLE_FORMAT,
+    ) -> Self:
+        samples = miniaudio._array_proto_from_format(sample_format)
+        samples.frombytes(
+            np.random.randint(0, 100, nsamples * nchannels, dtype=np.int16).tobytes()
+        )
+
+        audio = miniaudio.DecodedSoundFile(
+            name="",
+            nchannels=nchannels,
+            sample_rate=sample_rate,
+            samples=samples,
+            sample_format=sample_format,
+        )
+
+        return cls(audio)
+
+    def save(self, filename: str) -> None:
+        miniaudio.wav_write_file(filename, self._audio)
+
+    @property
+    def samples(self) -> np.ndarray:
+        return np.array(self._audio.samples, dtype=np.float32)
+
+    @property
+    def nchannels(self) -> int:
+        return self._audio.nchannels
+
+    @property
+    def duration(self) -> float:
+        return self._audio.duration
+
+    @property
+    def sample_rate(self) -> int:
+        return self._audio.sample_rate
+
+    @property
+    def num_frames(self) -> int:
+        return self._audio.num_frames
+
+    @property
+    def num_samples(self) -> int:
+        return len(self._audio.samples)
---
+++ tests/test_audio.py
@@ -0,0 +1,75 @@
+# mypy: allow-untyped-defs
+
+import numpy as np
+import pytest
+
+from simulst.audio import Audio
+
+
+@pytest.fixture
+def audio1ch():
+    sample_rate = 16000
+    nchannels = 1
+    nsamples = sample_rate * 4
+
+    return Audio.fake(nsamples, nchannels, sample_rate)
+
+
+@pytest.fixture
+def audio2ch():
+    sample_rate = 16000
+    nchannels = 2
+    nsamples = sample_rate * 4
+
+    return Audio.fake(nsamples, nchannels, sample_rate)
+
+
+def test_duration(audio1ch, audio2ch):
+    assert audio1ch.duration == audio2ch.duration
+
+
+def test_duration_1ch(audio1ch):
+    assert audio1ch.duration == 4.0
+
+
+def test_duration_2ch(audio2ch):
+    assert audio2ch.duration == 4.0
+
+
+def test_sample_rate(audio1ch, audio2ch):
+    assert audio1ch.sample_rate == audio2ch.sample_rate
+
+
+def test_num_samples(audio1ch, audio2ch):
+    assert audio1ch.num_samples * 2 == audio2ch.num_samples
+
+
+def test_num_frames(audio1ch, audio2ch):
+    assert audio1ch.num_frames == audio2ch.num_frames
+
+
+def test_from_bytes(audio1ch, tmp_path):
+    file_path = str(tmp_path / "test_audio.wav")
+
+    audio1ch.save(file_path)
+
+    with open(file_path, "rb") as f:
+        bytes = f.read()
+
+    audio_from_bytes = Audio.from_bytes(
+        bytes, nchannels=audio1ch.nchannels, sample_rate=audio1ch.sample_rate
+    )
+
+    np.testing.assert_array_equal(audio_from_bytes.samples, audio1ch.samples)
+
+
+def test_from_file(audio1ch, tmp_path):
+    file_path = str(tmp_path / "test_audio.wav")
+
+    audio1ch.save(file_path)
+
+    audio_from_file = Audio.from_file(
+        file_path, nchannels=audio1ch.nchannels, sample_rate=audio1ch.sample_rate
+    )
+
+    np.testing.assert_array_equal(audio_from_file.samples, audio1ch.samples)
